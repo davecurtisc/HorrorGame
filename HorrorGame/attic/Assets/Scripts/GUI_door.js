f927cc5809eea175237141c42d4792a155363220
davidcurtisc <davidcurtis@knights.ucf.edu>
2015-03-24 00:57:22 -0400

    Attic update. Imported models. Light flicker

---
+++ HorrorGame/attic/Assets/Scripts/GUI_door.js
@@ -0,0 +1,19 @@
+﻿var toggleGUI : boolean;
+
+
+ function Start() { 
+ toggleGUI = false;
+ }
+ 
+  function OnTriggerEnter (other : Collider) {
+  toggleGUI = true;
+  }
+  
+  function OnTriggerExit (other : Collider) {
+  toggleGUI = false;
+  }
+  
+  function OnGUI () {
+  	if (toggleGUI == true)
+  		GUI.Label (Rect (10, 300, 100, 20), "Press E to Enter");
+  }
---
+++ HorrorGame/attic/Assets/Scripts/GUI_key.js
@@ -0,0 +1,31 @@
+﻿
+var toggleGUI : boolean;
+var keycount = 0;
+
+
+ function Start() { 
+ toggleGUI = false;
+ }
+ 
+  function OnTriggerEnter (other : Collider) {
+  toggleGUI = true;
+  }
+  
+  function OnTriggerExit (other : Collider) {
+  toggleGUI = false;
+  }
+  
+  function OnGUI () {
+
+  
+  	if (toggleGUI == true)
+  		GUI.Label (Rect (10, 300, 100, 20), "Click to pickup the key");
+  	
+  	if(Input.GetMouseButtonDown(0)){
+  		Destroy(gameObject);
+  		GUI.Label (Rect (10, 300, 100, 20), "You have a KEY");
+ 		Debug.Log ("Key was picked up");
+ 		keycount = 1;
+  		}
+  }
+
