6d2298191e8a850d039944b9c0a943d4940fb27e
davidcurtisc <davidcurtis@knights.ucf.edu>
2015-04-15 08:44:12 -0400

    Puzzle 1 Complete

---
+++ HorrorGame/attic/Assets/Scripts/Box_searching.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box_searching : MonoBehaviour {
+
+	private bool withinRadius_crate;
+
+	public GameObject to_Search;
+	public GameObject found_Nothing;
+
+	public bool searching = false;
+
+	public bool engaged = false;
+
+	// Use this for initialization
+	void Start () {
+
+		to_Search.SetActive (false);
+		found_Nothing.SetActive (false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (withinRadius_crate == true && engaged == false) {
+			to_Search.SetActive (true);
+			
+			print ("should be told how to search");
+
+			searching = true;
+		}
+
+		if (searching == true && Input.GetKeyDown(KeyCode.E)) {
+			to_Search.SetActive (false);
+			//found_Nothing.SetActive (true);
+
+			engaged = true;
+			
+			print ("found nothing");
+		}
+
+		if (withinRadius_crate == true && Input.GetMouseButtonDown (1)) {
+			to_Search.SetActive (false);
+			found_Nothing.SetActive (false);
+			
+			//print ("should be told how to search");
+			
+			//searching = true;
+		}
+
+	
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+			withinRadius_crate = true;
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+			withinRadius_crate = false;
+	}
+}
---
+++ HorrorGame/attic/Assets/Scripts/Page_Turner.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Page_Turner : MonoBehaviour {
+
+	private bool withinRadius_book;
+
+	public GameObject page1;
+	public GameObject page2;
+	public GameObject page3;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+		page1.SetActive (false);
+		page2.SetActive (false);
+		page3.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (withinRadius_book == true && Input.GetMouseButtonDown (0)) {
+			page1.SetActive (true);
+
+			print ("should be reading");
+		}	
+
+		if(withinRadius_book == true && Input.GetKeyDown(KeyCode.N)){
+			page1.SetActive(false);
+			page2.SetActive(true);
+
+			print("should turn page");
+		}
+
+		if (withinRadius_book == true && Input.GetKeyDown (KeyCode.M)) {
+			page2.SetActive(false);
+			page3.SetActive(true);
+		}
+
+
+			if(Input.GetMouseButtonDown(1)){
+				page1.SetActive(false);
+				page2.SetActive(false);
+				page3.SetActive(false);
+			}
+		}
+
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+			withinRadius_book = true;
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+			withinRadius_book = false;
+	}
+}
+
