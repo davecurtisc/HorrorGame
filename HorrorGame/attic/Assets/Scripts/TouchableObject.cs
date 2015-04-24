52f857c3fc10523a63e0be009bf275e970cf79c7
davidcurtisc <davidcurtis@knights.ucf.edu>
2015-04-24 01:26:33 -0400

    Attic bug fixes 2
    - colliders on all attic objects
    - script fixes
    - room organization

--- HorrorGame/attic/Assets/Scripts/TouchableObject.cs
+++ HorrorGame/attic/Assets/Scripts/TouchableObject.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class TouchableObject : MonoBehaviour
 {
 	public GameObject displayImage;
+
+	public static bool read_Paper = false;
 	
 	void OnTouch()
 	{
 		displayImage.SetActiveRecursively(true);
 
 		print("touching");
+
+		read_Paper = true;
 	}
 	
 	void UnTouch()
