0cc02b964094073f85dc8900e7244f5efa500179
davidcurtisc <davidcurtis@knights.ucf.edu>
2015-04-24 23:05:43 -0400

    Hallway bug fixes 2
    - "[E]" to enter and "Locked" GUI added to all hallway scenes
    - Doors from hallway lead player to correct puzzle
    - All doors except the correct (green) door within the scene are locked

--- HorrorGame/attic/Assets/Scripts/Door_hallway3.cs
+++ HorrorGame/attic/Assets/Scripts/Door_hallway3.cs
@@ -2,21 +2,21 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Door_hallway : MonoBehaviour {
-
+public class Door_hallway3 : MonoBehaviour {
+	
 	private bool withinRadius_D1;
 	private bool withinRadius_D2;
 	private bool withinRadius_D3;
 	private bool withinRadius_D4;
 	private bool withinRadius_D5;
-
+	
 	public bool canopen = false;
 	
 	public GameObject how_Enter;
 	public GameObject door_Locked;
-
+	
 	//public AudioClip door_creak;
-
+	
 	// Use this for initialization
 	void Start () {
 		how_Enter.SetActive (false);
@@ -25,88 +25,89 @@ public class Door_hallway : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 		if (withinRadius_D1 == true) {
-
+			
 			canopen = true;
 		}
-
+		
 		if (withinRadius_D2 == true) {
-
+			
 			canopen = true;
 		}
-
+		
 		if (withinRadius_D3 == true) {
-
+			
 			canopen = true;
 		}
-
+		
 		if (withinRadius_D4 == true) {
-
+			
 			canopen = true;
 		}
-
+		
 		if (withinRadius_D5 == true) {
 			
 			canopen = true;
 		}
-
+		
 		// to open
-
+		
 		if (withinRadius_D1 == true && Input.GetKeyDown (KeyCode.E)) {
-
-
-			Application.LoadLevel ("Room1"); 
+			
+			
+			//Application.LoadLevel ("Room1"); 
 			//audio.PlayOneShot(door_creak, 1.0f);
+			
+			how_Enter.SetActive (false);
+			door_Locked.SetActive(true);
+			StartCoroutine(lockdoor_leaves());
 		}
-
+		
 		if (withinRadius_D2 == true && Input.GetKeyDown (KeyCode.E)) {
 			
 			
 			//Application.LoadLevel ("Room2"); 
 			//audio.PlayOneShot(door_creak, 1.0f);
-
+			
 			how_Enter.SetActive (false);
 			door_Locked.SetActive(true);
 			StartCoroutine(lockdoor_leaves());
 		}
-
+		
 		if (withinRadius_D3 == true && Input.GetKeyDown (KeyCode.E)) {
 			
 			
-			//Application.LoadLevel ("Tile_Puzzle"); 
+			Application.LoadLevel ("Tile_Puzzle"); 
 			//audio.PlayOneShot(door_creak, 1.0f);
 
-			how_Enter.SetActive (false);
-			door_Locked.SetActive(true);
-			StartCoroutine(lockdoor_leaves());
 		}
-
+		
 		if (withinRadius_D4 == true && Input.GetKeyDown (KeyCode.E)) {
 			
 			
 			//Application.LoadLevel ("First_Floor_Alpha"); 
 			//audio.PlayOneShot(door_creak, 1.0f);
-
+			
 			how_Enter.SetActive (false);
 			door_Locked.SetActive(true);
 			StartCoroutine(lockdoor_leaves());
 		}
-
+		
 		if (withinRadius_D5 == true && Input.GetKeyDown (KeyCode.E)) {
 			
 			
 			//Application.LoadLevel ("First_Floor_Alpha"); 
 			//audio.PlayOneShot(door_creak, 1.0f);
 			print("this door does not open");
-
+			
 			how_Enter.SetActive (false);
 			door_Locked.SetActive(true);
 			StartCoroutine(lockdoor_leaves());
 		}
-
+		
 	}
-
+	
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "D1") {
@@ -136,7 +137,7 @@ public class Door_hallway : MonoBehaviour {
 		if (other.gameObject.tag == "D1") {
 			withinRadius_D1 = false;
 			how_Enter.SetActive (false);
-
+			
 		}
 		if (other.gameObject.tag == "D2") {
 			withinRadius_D2 = false;
@@ -155,7 +156,7 @@ public class Door_hallway : MonoBehaviour {
 			how_Enter.SetActive (false);
 		}
 	}
-
+	
 	IEnumerator lockdoor_leaves()
 	{
 		
